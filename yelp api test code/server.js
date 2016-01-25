b1f844de1582dca078a473120bafb47182a11bb4
wohlfea <wohlfea@gmail.com>
2016-01-24 16:06:17 -0800

    triggering proxy via clientside js, server logs data

--- yelp api test code/server.js
+++ yelp api test code/server.js
@@ -2,6 +2,11 @@ var requestProxy = require('express-request-proxy'),
   express = require('express'),
   port = process.env.PORT || 3000,
   app = express();
+var oauthSignature = require('oauth-signature');
+var n = require('nonce')();
+var request = require('request');
+var qs = require('querystring');
+var _ = require('lodash');
 
 var proxyGitHub = function(request, response) {
   console.log('Routing GitHub request for', request.params[0]);
@@ -11,34 +16,18 @@ var proxyGitHub = function(request, response) {
   }))(request, response);
 };
 //////////
-
-/* require the modules needed */
-var oauthSignature = require('oauth-signature');
-var n = require('nonce')();
-var request = require('request');
-var qs = require('querystring');
-var _ = require('lodash');
-
-/* Function for yelp call
- * ------------------------
- * set_parameters: object with params to search
- * callback: callback(error, response, body)
- */
+var proxyYelp = function(request, response) {
+  console.log('Routing Yelp request for', request.url);
+  var qs_params = request.url.replace('/yelp/v2/search/?', '')
+  request_yelp(qs_params, response);
+}
 var request_yelp = function(set_parameters, callback) {
-
-  /* The type of request */
   var httpMethod = 'GET';
-
-  /* The url we are using for the request */
-  var url = 'http://api.yelp.com/v2/search';
-
-  /* We can setup default parameters here */
+  var url = 'http://api.yelp.com/v2/search'
   var default_parameters = {
     location: 'San+Francisco',
     sort: '2'
   };
-
-  /* We set the require parameters here */
   var required_parameters = {
     oauth_consumer_key : process.env.oauth_consumer_key,
     oauth_token : process.env.oauth_token,
@@ -47,38 +36,24 @@ var request_yelp = function(set_parameters, callback) {
     oauth_signature_method : 'HMAC-SHA1',
     oauth_version : '1.0'
   };
-
-  /* We combine all the parameters in order of importance */
   var parameters = _.assign(default_parameters, set_parameters, required_parameters);
-
-  /* We set our secrets here */
   var consumerSecret = process.env.consumerSecret;
   var tokenSecret = process.env.tokenSecret;
-
-  /* Then we call Yelp's Oauth 1.0a server, and it returns a signature */
-  /* Note: This signature is only good for 300 seconds after the oauth_timestamp */
   var signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
-
-  /* We add the signature to the list of paramters */
   parameters.oauth_signature = signature;
-
-  /* Then we turn the paramters object, to a query string */
   var paramURL = qs.stringify(parameters);
-
-  /* Add the query string to the url */
   var apiURL = url+'?'+paramURL;
-
-  /* Then we use request to send make the API Request */
   request(apiURL, function(error, response, body){
-    return callback(error, response, body);
+    console.log(body);
+    return (error, response, body);
   });
 
 };
-request_yelp('term=tacos&location=seattle', function(err, res, body) {
-  console.log(res);
-})
+// request_yelp('term=tacos&location=seattle', function(err, res, body) {
+//   console.log(res);
+// })
 //////////
-app.get('/github/*', proxyGitHub);
+app.get('/yelp/*', proxyYelp);
 
 app.use(express.static('./'));
 
