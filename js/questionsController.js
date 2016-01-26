009ed8a512c7f25bc16fae8a6d0125525874d326
henrybi <henrybi@u.washington.edu>
2016-01-26 09:55:18 -0800

    initializing second day

--- js/questionsController.js
+++ js/questionsController.js
@@ -3,27 +3,31 @@
   var questionsController = {};
 
   questionsController.index = function(){
+    $('#sidebar').hide();
     $('#map').hide();
     $('#landing').hide();
     $('#q1').show().siblings().hide();
-    $('#submit1').on('click', function(e){
-      e.preventDefault();
-      questionsController.term = $("#term").val();
-      console.log(questionsController.term);
-      $('#q1').hide();
-      $('#q2').show();
+
+
+    $('form').on('click', 'li.cs-selected span', function(){
+        console.log('what');
+        questionsController.term = $("#term").val();
+        console.log(questionsController.term);
+        $('#q1').hide();
+        $('#q2').show();
     });
+
 
     $('#submit2').on('click', function(e){
       e.preventDefault();
       questionsController.location = $("#location").val();
       console.log(questionsController.location);
+      $('#form').hide();
       yelp.ajaxCall(Location.loadAll);
       $('#map').show();
-
-      // eventually will route to result page after 2nd submit button
-
+      $('#sidebar').show();
     });
+
   };
 
     module.questionsController = questionsController;
