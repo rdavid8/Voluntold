3731aef52d301704e2353971f88f657c351dea24
henrybi <henrybi@u.washington.edu>
2016-01-25 16:26:37 -0800

    questions routes created.

--- js/yelp.js
+++ js/yelp.js
@@ -2,23 +2,24 @@
   // creating yelp object
   var yelp = {};
 // userInput method to save user input value as variables
-  yelp.userInput = function (callback){
-    $('#submit').on('click', function(e) {
-        e.preventDefault();
-        yelp.term = $('#term').val();
-        yelp.location = $('#location').val();
-        yelp.category = $('#category').val();
-        callback();
-    });
-  };
+  // yelp.userInput = function (callback){
+  //   $('#submit').on('click', function(e) {
+  //       e.preventDefault();
+  //       yelp.term = $('#term').val();
+  //       yelp.location = $('#location').val();
+  //       yelp.category = $('#category').val();
+  //       callback();
+  //   });
+  // };
 // ajax call method using saved variables
   yelp.ajaxCall = function (){
     $.ajax('yelp/v2/search/', {
       data: {
-        term: yelp.term,
-        location: yelp.location,
-        category_filter: yelp.category,
+        term: questionsController.term,
+        location: questionsController.location,
+        category_filter: 'nonprofit',
         limit: 10
+        // radius_filter = 15,
       },
       success: function(data) {
         manageDB.deleteTable(function(){});
@@ -33,7 +34,7 @@
     });
   };
 // when user clicks submit, run yelp.ajax as a callback function
-  yelp.userInput(yelp.ajaxCall);
+// yelp.userInput(yelp.ajaxCall);
 
   module.yelp = yelp;
 })(window);
