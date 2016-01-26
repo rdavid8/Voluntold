e28e5d170fd3d0bd60dcb89f78ae09bcaf2cc6c4
henrybi <henrybi@u.washington.edu>
2016-01-26 10:25:50 -0800

    css animate added

--- js/landingPageController.js
+++ js/landingPageController.js
@@ -5,12 +5,14 @@
   landingPageController.index = function(){
     $('#map').hide();
     $('#form').hide();
-
-
-
+    $('#sidebar').hide();
     $('body').on('click', function(){
       window.location.href = '/questions';
     })
+
+    $('#f1').addClass('animated slideInRight');
+    $('#f2').addClass('animated slideInLeft');
+    $('#bg').addClass('animated fadeIn');
 
   };
 
