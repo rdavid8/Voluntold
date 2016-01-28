e33b09817b0da0575a0dda298e22da926a1adad1
wohlfea <wohlfea@gmail.com>
2016-01-28 12:15:46 -0800

    yelp js is now linted

--- js/yelp.js
+++ js/yelp.js
@@ -1,17 +1,7 @@
 (function(module) {
   // creating yelp object
   var yelp = {};
-// userInput method to save user input value as variables
-  // yelp.userInput = function (callback){
-  //   $('#submit').on('click', function(e) {
-  //       e.preventDefault();
-  //       yelp.term = $('#term').val();
-  //       yelp.location = $('#location').val();
-  //       yelp.category = $('#category').val();
-  //       callback();
-  //   });
-  // };
-// ajax call method using saved variables
+
   yelp.ajaxCall = function (callback) {
     $.ajax('yelp/v2/search/', {
       data: {
@@ -19,7 +9,6 @@
         location: questionsController.location,
         category_filter: 'nonprofit',
         limit: 10
-        // radius_filter = 15,
       },
       success: function(data) {
         manageDB.deleteTable(function(){}); // Delete any previous data in database so we can fill it up again.
@@ -33,17 +22,14 @@
           console.log(JSON.parse(data));
           var bus = JSON.parse(data).businesses; //variable bus now houses business objects.
           $.each(bus, function(i){ // for every business in object. run this function
-            manageDB.populateDB(bus[i]) // Populate DB with each business obj
+            manageDB.populateDB(bus[i]); // Populate DB with each business obj
           });
         }
       },
       error: function() {
         console.log('There was an error');
       }
-    }).done(callback)
+    }).done(callback);
   };
-// when user clicks submit, run yelp.ajax as a callback function
-// yelp.userInput(yelp.ajaxCall);
-
   module.yelp = yelp;
 })(window);
