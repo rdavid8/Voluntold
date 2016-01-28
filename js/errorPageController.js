2438662fc7b3ae9b483f7dd01de6e3764e018e0f
Ryan <rdavid8@gmail.com>
2016-01-28 15:39:29 -0800

    errorpagecontroller

--- js/errorPageController.js
+++ js/errorPageController.js
@@ -4,16 +4,22 @@
 
   errorPageController.index = function(){
     back = true;
-    $('#landing').hide();
-    $('#questions').hide();
-    $('#sidebar').hide();
-    $('#map').hide();
+    errorPageController.hide();
+    errorPageController.show();
+  };
+  errorPageController.show = function(){
     $('#error').addClass('animated fadeIn').show();
     $('#e1').addClass('animated fadeInUp').show();
     $('#e2').addClass('animated fadeInDownBig').show();
     $('#bg3').addClass('animated fadeOut');
     $('#bg4').addClass('animated fadeIn').show();
     $('#shade2').addClass('animated fadeIn').show();
+  };
+  errorPageController.hide = function(){
+    $('#landing').hide();
+    $('#questions').hide();
+    $('#sidebar').hide();
+    $('#map').hide();
   };
 
   module.errorPageController = errorPageController;
